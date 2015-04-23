215a929073dd1e34386274dcd13b2b2334a83ab5
AJ <sirlevantai@gmail.com>
2015-04-23 10:20:16 +0700

    Initial Commit

---
+++ MyAssessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAssessment
+//
+//  Created by AJ on 4/23/15.
+//  Copyright (c) 2015 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
