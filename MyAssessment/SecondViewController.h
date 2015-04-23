9316713fb7558465b1276e796558afe4217fc455
AJ <sirlevantai@gmail.com>
2015-04-23 13:13:20 +0700

    first commit

---
+++ MyAssessment/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MyAssessment
+//
+//  Created by AJ on 4/23/15.
+//  Copyright (c) 2015 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
